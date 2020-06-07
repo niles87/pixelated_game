00112de5e0f6188ffdaacd7b96401700f645c544
niles87 <nbing19@gmail.com>
2020-06-06 17:14:09 -0700

    created classes for both npc and player characters and removed unneeded global variables, added functionality for npcs to come from left and right and respawn in random locations

--- script.js
+++ script.js
@@ -4,25 +4,147 @@ canvas.width = 800;
 canvas.height = 500;
 
 let keys = [];
+let npcActions = ["left", "right"];
+let npcs = 2;
 
-const player = {
-  x: 300,
-  y: 300,
-  width: 32,
-  height: 48,
-  frameX: 0,
-  frameY: 0,
-  speed: 9,
-  moving: false,
-};
-
+// Player image
 const playerSprite = new Image();
 playerSprite.src = "./images/ironman.png";
+
+// Background image
 const background = new Image();
 background.src = "./images/background.png";
 
+// NPC image
+const npcSprite = new Image();
+npcSprite.src = "./images/ronan.png";
+
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
   ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+
+function drawNPCSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+
+class Character {
+  constructor() {
+    this.width = 32;
+    this.height = 48;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.speed = 9;
+    this.moving = false;
+    this.x = 300;
+    this.y = 300;
+  }
+
+  draw() {
+    drawSprite(
+      playerSprite,
+      this.width * this.frameX,
+      this.height * this.frameY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+
+  update() {
+    this.movePlayer();
+    this.handlePlayerFrame();
+  }
+
+  movePlayer() {
+    if (keys["ArrowUp"] && this.y > 100) {
+      this.y -= this.speed;
+      this.frameY = 3;
+      this.moving = true;
+    }
+    if (keys["ArrowLeft"] && this.x > 0) {
+      this.x -= this.speed;
+      this.frameY = 1;
+      this.moving = true;
+    }
+    if (keys["ArrowRight"] && this.x < canvas.width - this.width) {
+      this.x += this.speed;
+      this.frameY = 2;
+      this.moving = true;
+    }
+    if (keys["ArrowDown"] && this.y < canvas.height - this.height) {
+      this.y += this.speed;
+      this.frameY = 0;
+      this.moving = true;
+    }
+  }
+
+  handlePlayerFrame() {
+    if (this.frameX < 3 && this.moving) {
+      this.frameX++;
+    } else {
+      this.frameX = 0;
+    }
+  }
+}
+
+class NPC {
+  constructor() {
+    this.x = 800;
+    this.y = 200;
+    this.width = 32;
+    this.height = 48;
+    this.frameX = 0;
+    this.frameY = 1;
+    this.speed = Math.random() * 1.5 + 8;
+    this.actions = npcActions[Math.floor(Math.random() * npcActions.length)];
+  }
+  draw() {
+    drawNPCSprite(
+      npcSprite,
+      this.width * this.frameX,
+      this.height * this.frameY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+  update() {
+    if (this.actions === "left") {
+      if (this.x > 0) {
+        this.x -= this.speed;
+      } else {
+        this.x = 800 + this.width;
+        this.y = Math.random() * (canvas.height - 100 - this.height) + 100;
+      }
+    } else if (this.actions === "right") {
+      this.frameY = 2;
+      if (this.x < canvas.width + this.width) {
+        this.x += this.speed;
+      } else {
+        this.x = 0 - this.width;
+        this.y = Math.random() * (canvas.height - 100 - this.height) + 100;
+      }
+    }
+    this.handleNPCFrame();
+  }
+  handleNPCFrame() {
+    if (this.frameX < 3) {
+      this.frameX++;
+    } else {
+      this.frameX = 0;
+    }
+  }
+}
+let npcArr = [];
+const player = new Character();
+for (let n = 0; n < npcs; n++) {
+  npcArr.push(new NPC());
 }
 
 window.addEventListener("keydown", function (e) {
@@ -34,37 +156,6 @@ window.addEventListener("keyup", function (e) {
   delete keys[e.key];
   player.moving = false;
 });
-
-function movePlayer() {
-  if (keys["ArrowUp"] && player.y > 100) {
-    player.y -= player.speed;
-    player.frameY = 3;
-    player.moving = true;
-  }
-  if (keys["ArrowLeft"] && player.x > 0) {
-    player.x -= player.speed;
-    player.frameY = 1;
-    player.moving = true;
-  }
-  if (keys["ArrowRight"] && player.x < canvas.width - player.width) {
-    player.x += player.speed;
-    player.frameY = 2;
-    player.moving = true;
-  }
-  if (keys["ArrowDown"] && player.y < canvas.height - player.height) {
-    player.y += player.speed;
-    player.frameY = 0;
-    player.moving = true;
-  }
-}
-
-function handlePlayerFrame() {
-  if (player.frameX < 3 && player.moving) {
-    player.frameX++;
-  } else {
-    player.frameX = 0;
-  }
-}
 
 let fps, fpsInterval, startTime, now, then, elapsed;
 
@@ -83,19 +174,12 @@ function animate() {
     then = now - (elapsed % fpsInterval);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(
-      playerSprite,
-      player.width * player.frameX,
-      player.height * player.frameY,
-      player.width,
-      player.height,
-      player.x,
-      player.y,
-      player.width,
-      player.height
-    );
-    movePlayer();
-    handlePlayerFrame();
+    for (let i = 0; i < npcArr.length; i++) {
+      npcArr[i].draw();
+      npcArr[i].update();
+    }
+    player.draw();
+    player.update();
   }
 }
 startAnimating(15);
